195b6701ef357fa97d0473bec29d71f37806940b
Andrés Cevallos <a.cevallos@qaamgo.com>
2015-07-22 17:08:57 +0200

    Added JS file

---
+++ jquery.donetyping.js
@@ -0,0 +1,39 @@
+;(function($){
+    $.fn.extend({
+        donetyping: function(callback,timeout){
+            timeout = timeout || 1e3; // 1 second default timeout
+            var timeoutReference,
+                doneTyping = function(el){
+                    if (!timeoutReference) return;
+                    timeoutReference = null;
+                    callback.call(el);
+                };
+            return this.each(function(i,el){
+                var $el = $(el);
+
+                $el.is(':input') || $el.is('textarea') && $el.on('keyup keypress',function(e){
+                    var excludedKeys = [
+                        16, 17, 18, 20, 35, 36, 37,
+                        38, 39, 40, 45, 144, 225
+                    ];
+                    // This catches the backspace button in chrome, but also prevents
+                    // the event from triggering too premptively. Without this line,
+                    // using tab/shift+tab will make the focused element fire the callback.
+                    if (e.type=='keyup' &&  $.inArray( e.keyCode, excludedKeys ) !== -1) return;
+
+                    // Check if timeout has been set. If it has, "reset" the clock and
+                    // start over again.
+                    if (timeoutReference) clearTimeout(timeoutReference);
+                    timeoutReference = setTimeout(function(){
+                        // if we made it here, our timeout has elapsed. Fire the
+                        // callback
+                        doneTyping(el);
+                    }, timeout);
+                }).on('blur',function(){
+                    // If we can, fire the event since we're leaving the field
+                    doneTyping(el);
+                });
+            });
+        }
+    });
+})(jQuery);
